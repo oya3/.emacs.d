e8f71fea9c468c55bb7c8a67c7e6fa87e913aa6a
Kazunori OYA <terje@oya3.net>
2023-05-15 13:01:24 +0900

    refactoring

--- init.el
+++ init.el
@@ -712,7 +712,7 @@ mouse-3: delete other windows"
               ("C-x i n" . yas-new-snippet)
               ("C-x i v" . yas-visit-snippet-file)
               ("C-x i l" . yas-describe-tables)
-              ("C-x i g" . yas-reload-all))              
+              ("C-x i g" . yas-reload-all))
   :config
   (yas-global-mode 1)
   (setq yas-prompt-functions '(yas-ido-prompt))
