1d0576144d739304e576b873d5acd21b7bf2b45c
Kazunori OYA <kazunori_oya@nsware.co.jp>
2025-03-07 17:30:08 +0900

    update scss-mode for version 30.1

--- init.el
+++ init.el
@@ -1196,6 +1196,12 @@ mouse-3: delete other windows"
 
 
 ;; scss-modeの設定
+(when (>= emacs-major-version 30)
+  (setq flymake-allowed-file-name-masks
+        '((".+\\.scss$" flymake-scss-init)))
+  (setq flymake-err-line-patterns
+        '(("on line \\([0-9]+\\) of \\([^ ]+\\)$" 2 1 nil 2))))
+
 (use-package scss-mode
   :ensure t
   :config
@@ -1339,8 +1345,7 @@ mouse-3: delete other windows"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(robe jedi yaml-mode markdown-mode py-autopep8 elpy omnisharp csharp-mode company-irony company-irony-c-headers company irony yasnippet magit git-gutter+ dumb-jump symbol-overlay counsel fzf wgrep pcre2el visual-regexp-steroids visual-regexp tabbar neotree all-the-icons dracula-theme use-package)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
