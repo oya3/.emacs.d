972f2f1ac60872636c00b90c47ae95c5af37eb18
Kazunori OYA <kazunori.oya3@gmail.com>
2025-04-30 12:59:46 +0900

    refactoring

--- setup_elpy.sh
+++ setup_elpy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pip install jedi flake8 importmagic autopep8 yapf black
+pip install jedi flake8 importmagic autopep8 yapf black python-lsp-server
+npm install -g typescript-language-server typescript vls @volar/vue-language-server 
